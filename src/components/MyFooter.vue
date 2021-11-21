18d0c0695555af57b400b7504759983b90937f09
111sugar <1853391862@qq.com>
2021-11-21 13:38:17 +0800

    message

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="todo-footer" v-show="total">
+    <label>
+      <input type="checkbox" :checked="isAll" @change="changeAll"/>
+    </label>
+    <span> <span>已完成{{doneTotal}}</span> /全部{{total}} </span>
+    <button class="btn btn-danger" @click="clearAll">清除已完成任务</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "MyFooter",
+  props:['todos','checkAllTodo','clearAllTodo'],
+  computed:{
+    total(){
+      return this.todos.length;
+    },
+    isAll(){
+      return this.doneTotal===this.total && this.total>0
+    },
+    doneTotal(){
+      let i=0;
+      this.todos.forEach(todo => {
+        if(todo.done){i++}
+      });
+      return i;
+    }
+  },
+  methods:{
+    changeAll(e){
+      this.checkAllTodo(e.target.checked)
+    },
+    clearAll(){
+      this.clearAllTodo()
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todo-footer i{
+height: 40px;
+line-height: 40px;
+padding-left: 6рх;
+margin-top: 5px;}
+.todo-footer label {
+display: inline-block;
+margin-right: 20px;
+cursor: pointer;}
+.todo-footer label input {
+position: relative ;
+top: -1px;
+vertical-align: middle;
+margin-right: 5px;}
+.todo-footer button{
+float: right;
+margin-top: 5px;}
+</style>
